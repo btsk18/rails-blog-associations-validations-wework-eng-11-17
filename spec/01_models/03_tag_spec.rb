3df956e8835b28ac375dc71aa0863d917a8274e6
Brandon Leeds <brandonleeds@gmail.com>
2018-03-25 21:59:21 +0000

    automatically backed up by learn

--- spec/01_models/03_tag_spec.rb
+++ spec/01_models/03_tag_spec.rb
@@ -14,11 +14,11 @@ describe Tag do
   #   let(:tag) { Tag.create(name: 'cute') }
   #   let(:user) { User.create(name: 'kitten') }
   #   let(:post) { Post.create(user: user, name: 'post', content: 'content') }
-  # 
+  #
   #   before do
   #     PostTag.create(tag: tag, post: post)
   #   end
-  # 
+  #
   #   it 'has many posts' do
   #     expect(tag.posts.count).to eq(1)
   #   end
